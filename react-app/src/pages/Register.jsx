bb75d9abc12938e22877fd58c6dd0c67a0701396
bingqianyang1 <bingqianyang1@ccs.neu.edu>
2022-12-09 22:41:59 -0800

    Register finished

--- react-app/src/pages/Register.jsx
+++ react-app/src/pages/Register.jsx
@@ -1,35 +1,67 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Background from '../components/Background';
 import Header from '../components/Header';
+import { firebaseAuth } from '../utils/firebase-config';
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [registerVal, setRegisterVal] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleRegister = async () => {
+    try {
+      const { email, password } = registerVal;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) {
+      navigate("/")
+    };
+  });
+
   return (
     <Container>
       <Background />
       <div className="content">
         <Header login/>
         <div className="body flex column align-center justify-center">
-            <div className="text flex column">
+            <div className='text'>
                 <h1>Get access to thousands of videos here.</h1>
                 <h4>
                 Ready to explore? Enter your email to register.
                 </h4>
             </div>
 
-            <div>
-                <input type="email" name = "email" placeholder="Email Address" />
+            <div className='form'>
+                <input type="email" name = "email" placeholder="Email Address" 
+                value={registerVal.email} 
+                onChange={(event) => setRegisterVal({...registerVal, [event.target.name]: event.target.value})} />
             </div>
-            <div>
-                <input type="password" name = "password" placeholder="Password" />
+            <div className='form'>
+                <input type="password" name = "password" placeholder="Password" 
+                value={registerVal.password} 
+                onChange={(event) => setRegisterVal({...registerVal, [event.target.name]: event.target.value})} />
             </div>
+        
             <div>
-                <button>Register</button>
+                <button onClick={handleRegister}>
+                  Register
+                </button>
             </div>
+
             <div>
-              <h6>
+              <h4>
                 Have an account? Log in here.
-              </h6>
+              </h4>
             </div>
 
             <button>Login</button>
@@ -40,13 +72,34 @@ export default function Register() {
 }
 
 const Container = styled.div`
-    position: relative;
-    .content {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    display: grid;
-    grid-template-rows: 15vh 80vh;
+position: relative;
+.content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 15vh 70vh;
+  .body {
+    gap: 1.5rem;
+    .form {
+      input {
+        padding: 1rem;
+        font-size: 1.2rem;
+        border: none;
+      }
+    }
+    button {
+      padding: 0.5rem 1rem;
+      background-color: white;
+      cursor: pointer;
+      color: black;
+      border-radius: 0.2rem;
+      font-weight: bolder;
+      font-size: 1.1rem;
+    }
+  }
+}
+ 
 `;
